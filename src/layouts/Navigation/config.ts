10adfe051ab11a557253c4ce23b2959fd19a4066
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-05 12:45:46 +0200

    chore: change Link types nad update links data structure

--- src/layouts/Navigation/config.ts
+++ src/layouts/Navigation/config.ts
@@ -1,7 +1,7 @@
 import { Links } from 'types';
 
 export const config: Links = [
-  { name: 'Home', path: '/' },
-  { name: 'About', path: '/about' },
-  { name: 'Blog', path: '/blog' }
+  { name: 'Home', path: '/', shouldShow: false },
+  { name: 'About', path: '/about', shouldShow: true },
+  { name: 'Blog', path: '/blog', shouldShow: true }
 ];
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 export type Link = {
   name: 'About' | 'Blog' | 'Home';
   path: string;
+  shouldShow: boolean;
 };
 
 export type Links = Link[];
