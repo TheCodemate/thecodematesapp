d63def3ebe34838ea7e3df831a6112462854410f
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-06 11:49:15 +0200

    add: Footer to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Footer } from 'layouts/Navigation/Footer/Footer';
 import { Header } from 'layouts/Navigation/Header/Header';
 export const App: React.FC = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
