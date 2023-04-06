08b59de27c29cbaa334387638d8414cc5b82e6a8
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-06 19:25:19 +0200

    feat: implement react-hook-form logic

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,29 +1,53 @@
-import React, { FormEvent } from 'react';
+import React, { SyntheticEvent } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
 import styles from './Form.module.scss';
 
-type FormProps = {};
+type FormProps = {
+  toggleModalHandler: () => void;
+};
 
-export const Form: React.FC<FormProps> = () => {
-  const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(e);
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(10)
+});
+
+const formConfig = {
+  defaultValues: {
+    email: '',
+    password: ''
+  },
+  resolver: zodResolver(schema)
+};
+
+export const Form: React.FC<FormProps> = ({ toggleModalHandler }) => {
+  const { register, handleSubmit } = useForm(formConfig);
+
+  const onSubmitHandler = (data: FieldValues) => {
+    console.log('submitted: ', data);
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.close}></div>
+    <div
+      className={styles.container}
+      //on click has to be added to prevent eventPropagation (bubbling). If not present clicking on any Component's element will close the modal
+      onClick={(e: SyntheticEvent<HTMLDivElement>) => e.stopPropagation()}>
+      <div onClick={toggleModalHandler} className={styles.close}></div>
 
       <div className={styles.logoContainer}>
         <p className={styles.logo}>A</p>
       </div>
       <h3 className={styles.heading}>Welcome to Adoptme 😻</h3>
       <div className="formWrapper">
-        <form className={styles.form} method="post" onSubmit={(e) => onSubmitHandler(e)}>
+        <form className={styles.form} method="post" onSubmit={handleSubmit(onSubmitHandler)}>
           <div className={styles.inputContainer}>
             <label className={styles.label} htmlFor="email">
               Email:
             </label>
             <input
+              {...register('email', { required: true })}
               className={styles.input}
               type="email"
               name="email"
@@ -36,6 +60,7 @@ export const Form: React.FC<FormProps> = () => {
               Password:
             </label>
             <input
+              {...register('password', { required: true })}
               className={styles.input}
               type="password"
               name="password"
