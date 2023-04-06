b1c4b8c86de83ffe1306e6db2ecfa34825059d5f
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-06 19:21:44 +0200

    refactor: rename prop

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,17 +5,17 @@ import styles from './Modal.module.scss';
 type ModalProps = {
   children?: ReactElement | ReactElement[];
   isOpen: boolean;
-  toggleHandler: () => void;
+  toggleModalHandler: () => void;
 };
 
-export const Modal: React.FC<ModalProps> = ({ isOpen, children, toggleHandler }) => {
+export const Modal: React.FC<ModalProps> = ({ isOpen, children, toggleModalHandler }) => {
   if (!isOpen) {
     return null;
   }
 
   return (
     <div className={styles.container}>
-      <div onClick={toggleHandler} className={styles.backdrop}>
+      <div onClick={toggleModalHandler} className={styles.backdrop}>
         {children}
       </div>
     </div>
