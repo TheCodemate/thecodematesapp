4e07ba7b42756d8649938e713809bab42ac27dbd
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-06 10:07:41 +0200

    refactor: move pops type to component's tsx file

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,12 @@
-import { ButtonProps } from './Button.types';
-
+import { ReactNode } from 'react';
 import styles from './Button.module.scss';
+
+export type ButtonProps = {
+  children: ReactNode;
+  onClick?: () => void;
+  style?: React.HTMLAttributes<HTMLButtonElement> | undefined;
+  variant?: 'action' | 'outlined' | 'disabled' | 'default';
+};
 
 export const Button: React.FC<ButtonProps> = ({
   children,
