da7e0c00f9ec741a99271f6b1ee0afc082d5ed11
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-07 16:43:46 +0200

    feat: create Layout comp

---
+++ src/layouts/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import { useModal } from 'components/Modal/hooks';
+
+import { Modal } from 'components/Modal/Modal';
+import { Form } from 'components/Form/Form';
+
+import { Footer } from 'layouts/Navigation/Footer/Footer';
+import { Header } from 'layouts/Navigation/Header/Header';
+import { Content } from 'layouts/Content/Content';
+
+import styles from './Layout.module.scss';
+
+export const Layout = () => {
+  const { isModalOpen, toggleModalHandler } = useModal();
+
+  return (
+    <div className={styles.container}>
+      <Header toggleModalHandler={toggleModalHandler} />
+      <Modal toggleModalHandler={toggleModalHandler} isOpen={isModalOpen}>
+        <Form toggleModalHandler={toggleModalHandler} />
+      </Modal>
+      <Content />
+      <Footer />
+    </div>
+  );
+};
