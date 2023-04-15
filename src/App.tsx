2f72ca76c107a86bf454b8705bca0437cb2f1fe9
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-15 16:10:31 +0200

    refactor: lift up all components from Layout to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { Layout } from '@layouts/Layout/Layout';
+import { Header } from '@layouts/Navigation/Header/Header';
+import { Content } from '@layouts/Content/Content';
+import { Modal } from '@components/Modal/Modal';
+import { Form } from '@components/Form/Form';
+import { useModal } from '@components/Modal/hooks';
 
 export const App = () => {
-  return <Layout />;
+  const { isModalOpen, toggleModalHandler } = useModal();
+  return (
+    <Layout isFooterVisible={false}>
+      <Header toggleModalHandler={toggleModalHandler} />
+      <Modal toggleModalHandler={toggleModalHandler} isOpen={isModalOpen}>
+        <Form toggleModalHandler={toggleModalHandler} />
+      </Modal>
+      <Content toggleModalHandler={toggleModalHandler} />
+    </Layout>
+  );
 };
