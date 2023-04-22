703142cac942593231aeab3f8a849a8e38972ba3
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-22 10:07:13 +0200

    feat: implement SearchPage and PetPage into app

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import { Home } from './pages/Home/Home';
 import { TermsOfService } from './pages/TermsOfService/TermsOfService';
 import { PrivacyPolicy } from './pages/PrivacyPolicy/PrivacyPolicy';
 import { NotFound } from './pages/NotFoundPage/NotFoundPage';
+import { SearchPage } from './pages/SearchPage/SearchPage';
+import { PetPage } from './pages/PetPage/PetPage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/terms-of-service" element={<TermsOfService />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="terms-of-service" element={<TermsOfService />} />
+        <Route path="privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="adopt">
+          <Route index element={<SearchPage />} />
+          <Route path=":id" element={<PetPage />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
