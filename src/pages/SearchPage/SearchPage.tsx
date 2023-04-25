1a342eaf62b5741be18234067aeae9d2a847e415
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-25 17:54:48 +0200

    feat: add SearchPage

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,30 +1,29 @@
-import { SearchCard } from '@/components/SearchCard/SearchCard';
-import dog from '../../../assets/images/dog-small.jpg';
+import { usePets } from '@/utils/usePets';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 import styles from './SearchPage.module.scss';
+import { SearchCardsList } from '@/components/SearchCardsList/SearchCardsList';
 
 export const SearchPage = () => {
-  console.log(<SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />);
+  const { pets, isLoading, error } = usePets();
+
+  if (isLoading) {
+    return (
+      <Box>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (!pets) {
+    return <h2>{error}</h2>;
+  }
 
   return (
     <div className={styles.main}>
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/alvan-nee-8g0D8ZfFXyA-unsplash.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/alvan-nee-eoqnr8ikwFE-unsplash (1).jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/karsten-winegeart-oU6KZTXhuvk-unsplash.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
-      <SearchCard bgUrl={'../../../assets/images/dog-small.jpg'} />
+      <h1 className={styles.heading}>Scroll down to find you&apos;re loved one!</h1>
+      <SearchCardsList data={pets} />
     </div>
   );
 };
