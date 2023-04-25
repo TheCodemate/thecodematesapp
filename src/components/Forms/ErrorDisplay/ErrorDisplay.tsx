5d723eac0db2df2f6c8d35c12696cf043ceb3b62
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-25 22:42:46 +0200

    refactor: ErrorDisplay

--- src/components/Forms/ErrorDisplay/ErrorDisplay.tsx
+++ src/components/Forms/ErrorDisplay/ErrorDisplay.tsx
@@ -2,13 +2,8 @@ import styles from './ErrorDisplay.module.scss';
 
 type Props = {
   errorMessage: string;
-  ariaId: string;
 };
 
-export const ErrorDisplay = ({ errorMessage, ariaId }: Props) => {
-  return (
-    <span id={ariaId} className={styles.errorMessage}>
-      {errorMessage}
-    </span>
-  );
+export const ErrorDisplay = ({ errorMessage }: Props) => {
+  return <span className={styles.errorMessage}>{errorMessage}</span>;
 };
