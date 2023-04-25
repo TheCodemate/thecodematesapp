9e18bc4e6fe311d462076bf2d93366737c5205b9
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-25 22:43:58 +0200

    refactor: Layout

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -6,41 +6,54 @@ import { Footer } from '@components/Navigation/Footer/Footer';
 import { Modal } from '@components/Modal/Modal';
 
 import styles from './Layout.module.scss';
-import { LoginForm } from '@/components/Forms/LoginForm/LoginForm';
-import { RegisterForm } from '@/components/Forms/RegisterForm/RegisterForm';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { useState } from 'react';
+import { FormLayout } from '@/components/Forms/FormLayout/FormLayout';
 
 type ContextType = {
   isModalOpen: boolean;
   toggleModalHandler: () => void;
   isRegisterRequested: boolean;
-  setIsRegisterRequested: Dispatch<SetStateAction<boolean>>;
+  openRegisterFormHandler: () => void;
+  closeRegisterFormHandler: () => void;
 };
 
 export const Layout = () => {
   const { isModalOpen, toggleModalHandler } = useModal();
   const [isRegisterRequested, setIsRegisterRequested] = useState(false);
+
+  const openRegisterFormHandler = () => {
+    setIsRegisterRequested(true);
+  };
+
+  const closeRegisterFormHandler = () => {
+    setIsRegisterRequested(false);
+  };
+
   return (
     <div className={styles.container}>
-      <Header toggleModalHandler={toggleModalHandler} />
+      <Header
+        toggleModalHandler={toggleModalHandler}
+        openRegisterFormHandler={openRegisterFormHandler}
+        closeRegisterFormHandler={closeRegisterFormHandler}
+      />
       <Outlet
-        context={{ isModalOpen, toggleModalHandler, isRegisterRequested, setIsRegisterRequested }}
+        context={{
+          isModalOpen,
+          toggleModalHandler,
+          isRegisterRequested,
+          openRegisterFormHandler,
+          closeRegisterFormHandler
+        }}
       />
       <Footer />
       <Modal isOpen={isModalOpen} toggleModalHandler={toggleModalHandler}>
-        {isRegisterRequested ? (
-          <RegisterForm
-            toggleModalHandler={toggleModalHandler}
-            showCloseButton={true}
-            registrationRequestedHandler={setIsRegisterRequested}
-          />
-        ) : (
-          <LoginForm
-            toggleModalHandler={toggleModalHandler}
-            showCloseButton={true}
-            registrationRequestedHandler={setIsRegisterRequested}
-          />
-        )}
+        <FormLayout
+          isRegisterRequested={isRegisterRequested}
+          openRegisterFormHandler={openRegisterFormHandler}
+          closeRegisterFormHandler={closeRegisterFormHandler}
+          toggleModalHandler={toggleModalHandler}
+          showCloseButton
+        />
       </Modal>
     </div>
   );
