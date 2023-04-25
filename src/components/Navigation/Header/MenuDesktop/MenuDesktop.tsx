a3826f94f857d570302743aff150d106c5da609c
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-25 22:43:04 +0200

    refactor: Menu components

--- src/components/Navigation/Header/MenuDesktop/MenuDesktop.tsx
+++ src/components/Navigation/Header/MenuDesktop/MenuDesktop.tsx
@@ -2,26 +2,43 @@ import { Button } from '@components/Button/Button';
 import styles from './MenuDesktop.module.scss';
 
 type Props = {
+  openRegisterFormHandler: () => void;
+  closeRegisterFormHandler: () => void;
   toggleModalHandler: () => void;
 };
 
-export const MenuDesktop = ({ toggleModalHandler }: Props) => (
-  <nav className={styles.navigationContainer}>
-    <ul className={styles.links}>
-      <li>
-        {/* needs to be changed when react-router-dom will be implemented */}
-        <p>About</p>
-      </li>
-      <li>
-        {/* needs to be changed when react-router-dom will be implemented */}
-        <p>Blog</p>
-      </li>
-    </ul>
-    <div className={styles.buttonContainer}>
-      <Button onClick={toggleModalHandler} variant={'action'}>
-        Log in
-      </Button>
-      <Button onClick={toggleModalHandler}>Sign up</Button>
-    </div>
-  </nav>
-);
+export const MenuDesktop = ({
+  openRegisterFormHandler,
+  closeRegisterFormHandler,
+  toggleModalHandler
+}: Props) => {
+  const handleLoginOnClick = () => {
+    toggleModalHandler();
+    closeRegisterFormHandler();
+  };
+
+  const handleRegisterOnClick = () => {
+    toggleModalHandler();
+    openRegisterFormHandler();
+  };
+  return (
+    <nav className={styles.navigationContainer}>
+      <ul className={styles.links}>
+        <li>
+          {/* needs to be changed when react-router-dom will be implemented */}
+          <p>About</p>
+        </li>
+        <li>
+          {/* needs to be changed when react-router-dom will be implemented */}
+          <p>Blog</p>
+        </li>
+      </ul>
+      <div className={styles.buttonContainer}>
+        <Button onClick={handleLoginOnClick} variant={'action'}>
+          Log in
+        </Button>
+        <Button onClick={handleRegisterOnClick}>Sign up</Button>
+      </div>
+    </nav>
+  );
+};
