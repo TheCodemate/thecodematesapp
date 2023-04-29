7d3513185e32d97e98790972abdc2a66f1af4970
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-04-29 13:32:57 +0200

    feat: create announcementsSlice

---
+++ src/store/features/announcements/announcements.slice.ts
@@ -0,0 +1,37 @@
+import { PetsType } from '@/types';
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchAnnouncements } from './announcements.thunks';
+
+type InitialStateType = {
+  announcements: PetsType;
+  errorMessage: string | undefined;
+  isLoading: boolean;
+};
+
+const initialState: InitialStateType = {
+  announcements: [],
+  errorMessage: '',
+  isLoading: false
+};
+
+const announcementsSlice = createSlice({
+  name: 'announcements',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAnnouncements.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchAnnouncements.fulfilled, (state, action) => {
+      state.announcements = [...action.payload];
+      state.isLoading = false;
+    });
+    builder.addCase(fetchAnnouncements.rejected, (state, action) => {
+      state.errorMessage = action.payload;
+      state.isLoading = false;
+    });
+  }
+});
+
+export const {} = announcementsSlice.actions;
+export default announcementsSlice;
