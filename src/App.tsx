2ab75dfe0ac263d8f195b5dfc641348f17540ca9
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-05-08 20:20:26 +0200

    feat: add page responsible for adding announcements to the router

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,28 @@ import { PrivacyPolicy } from './pages/PrivacyPolicy/PrivacyPolicy';
 import { NotFound } from './pages/NotFoundPage/NotFoundPage';
 import { SearchPage } from './pages/SearchPage/SearchPage';
 import { PetPage } from './pages/PetPage/PetPage';
+import { AddAnnouncementPage } from './pages/AddAnnouncementPage/AddAnnouncementPage';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index path="/" element={<Home />} />
-        <Route path="terms-of-service" element={<TermsOfService />} />
-        <Route path="privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="adopt">
-          <Route index element={<SearchPage />} />
-          <Route path=":id" element={<PetPage />} />
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path="/" element={<Home />} />
+          <Route path="terms-of-service" element={<TermsOfService />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="adopt">
+            <Route index element={<SearchPage />} />
+            <Route path=":id" element={<PetPage />} />
+            <Route path="add-new" element={<AddAnnouncementPage />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
